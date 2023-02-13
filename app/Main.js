fa8b5cdf0c689d0753c7e8d5b5ea0e43e73ee952
Pedro C. Pacheco <cpedropacheco@hotmail.com>
2023-02-13 05:53:28 -0300

    signIn and signOut buttons and relatable headers

--- app/Main.js
+++ app/Main.js
@@ -1,13 +1,13 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
-// My Components
-import Header from "./components/Header"
-import HomeGuest from "./components/HomeGuest"
-import Footer from "./components/Footer"
-import About from "./components/About"
-import Terms from "./components/Terms"
+//Components
+import Header from "./components/Header";
+import HomeGuest from "./components/HomeGuest";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import Terms from "./components/Terms";
 
 function Main() {
   return (
@@ -20,12 +20,12 @@ function Main() {
       </Routes>
       <Footer />
     </BrowserRouter>
-  )
+  );
 }
 
-const root = ReactDOM.createRoot(document.querySelector("#app"))
-root.render(<Main />)
+const root = ReactDOM.createRoot(document.querySelector("#app"));
+root.render(<Main />);
 
 if (module.hot) {
-  module.hot.accept()
+  module.hot.accept();
 }
