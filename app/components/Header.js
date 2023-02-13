46bd47c5f3254c2254a8c087eb53be7b235ba71c
Pedro C. Pacheco <cpedropacheco@hotmail.com>
2023-02-12 22:15:16 -0300

    first commit

---
+++ app/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import HeaderLoggedOut from "./HeaderLoggedOut";
+
+function Header() {
+  return (
+    <header className="header-bar bg-primary mb-3">
+      <div className="container d-flex flex-column flex-md-row align-items-center p-3">
+        <h4 className="my-0 mr-md-auto font-weight-normal">
+          <Link to="/" className="text-white">
+            ComplexApp
+          </Link>
+        </h4>
+        <HeaderLoggedOut />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
